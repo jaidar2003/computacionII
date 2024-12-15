cf88d0a1431d6b481ef5c9cf7e2fcbf1d6968dcd
juanma <j.aidar@alumno.um.edu.ar>
2024-12-15 17:48:29 -0300

    feat: optimizar intercambio de archivos en servidor concurrente fix: evitar bloqueos en recepción de datos del cliente

--- proyecto/cliente/cliente.py
+++ proyecto/cliente/cliente.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 class ClienteArchivos:
     def __init__(self, host='localhost', puerto=8080):
@@ -9,35 +10,47 @@ class ClienteArchivos:
 
     def listar_archivos(self):
         self.socket_cliente.sendall('listar'.encode())
-        respuesta = self.socket_cliente.recv(1024).decode()
-        print(respuesta)
+        respuesta = self.socket_cliente.recv(4096).decode()
+        print('Archivos disponibles:\n' + respuesta)
 
     def descargar_archivo(self, nombre_archivo):
         self.socket_cliente.sendall(f'descargar {nombre_archivo}'.encode())
-        with open(nombre_archivo, 'wb') as archivo:
-            while True:
-                datos = self.socket_cliente.recv(1024)
-                if not datos:
-                    break
-                archivo.write(datos)
-        print('Archivo descargado correctamente')
+        confirmacion = self.socket_cliente.recv(1024).decode()
+        if confirmacion == 'OK':
+            with open(nombre_archivo, 'wb') as archivo:
+                while True:
+                    datos = self.socket_cliente.recv(1024)
+                    if datos.endswith(b'FIN'):
+                        archivo.write(datos[:-3])
+                        break
+                    archivo.write(datos)
+            print('Archivo descargado correctamente.')
+        else:
+            print(confirmacion)
 
     def subir_archivo(self, nombre_archivo):
+        if not os.path.isfile(nombre_archivo):
+            print('El archivo no existe.')
+            return
         self.socket_cliente.sendall(f'subir {nombre_archivo}'.encode())
-        with open(nombre_archivo, 'rb') as archivo:
-            datos = archivo.read()
-            self.socket_cliente.sendall(datos)
-        respuesta = self.socket_cliente.recv(1024).decode()
-        print(respuesta)
+        confirmacion = self.socket_cliente.recv(1024).decode()
+        if confirmacion == 'OK':
+            with open(nombre_archivo, 'rb') as archivo:
+                while chunk := archivo.read(1024):
+                    self.socket_cliente.sendall(chunk)
+            self.socket_cliente.sendall(b'FIN')
+            respuesta = self.socket_cliente.recv(1024).decode()
+            print(respuesta)
 
     def salir(self):
         self.socket_cliente.sendall('salir'.encode())
         self.socket_cliente.close()
+        print('Desconectado del servidor.')
 
 if __name__ == '__main__':
     cliente = ClienteArchivos()
     while True:
-        print('Opciones:')
+        print('\nOpciones:')
         print('1. Listar archivos')
         print('2. Descargar archivo')
         print('3. Subir archivo')
@@ -46,5 +59,13 @@ if __name__ == '__main__':
         if opcion == '1':
             cliente.listar_archivos()
         elif opcion == '2':
-            nombre_archivo = input('Ingrese el nombre del archivo: ')
+            nombre_archivo = input('Ingrese el nombre del archivo a descargar: ')
             cliente.descargar_archivo(nombre_archivo)
+        elif opcion == '3':
+            nombre_archivo = input('Ingrese el nombre del archivo a subir: ')
+            cliente.subir_archivo(nombre_archivo)
+        elif opcion == '4':
+            cliente.salir()
+            break
+        else:
+            print('Opción inválida.')
--- proyecto/servidor/servidor.py
+++ proyecto/servidor/servidor.py
@@ -9,53 +9,74 @@ class ServidorArchivos:
         self.socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket_servidor.bind((self.host, self.puerto))
         self.socket_servidor.listen(5)
-        self.archivos = {}
+        self.directorio_archivos = 'archivos'  # Directorio donde se almacenan los archivos
 
-    def manejar_conexion(self, cliente_socket):
-        while True:
-            mensaje = cliente_socket.recv(1024).decode()
-            if mensaje == 'listar':
-                self.listar_archivos(cliente_socket)
-            elif mensaje.startswith('descargar '):
-                self.descargar_archivo(cliente_socket, mensaje[10:])
-            elif mensaje.startswith('subir '):
-                self.subir_archivo(cliente_socket, mensaje[6:])
-            elif mensaje == 'salir':
-                break
-            else:
-                cliente_socket.sendall('Comando no reconocido'.encode())
+    def manejar_conexion(self, cliente_socket, direccion):
+        print(f'Conexión establecida con {direccion}')
+        try:
+            while True:
+                mensaje = cliente_socket.recv(1024).decode()
+                if not mensaje:
+                    break
+                if mensaje == 'listar':
+                    self.listar_archivos(cliente_socket)
+                elif mensaje.startswith('descargar '):
+                    nombre_archivo = mensaje.split(' ', 1)[1]
+                    self.descargar_archivo(cliente_socket, nombre_archivo)
+                elif mensaje.startswith('subir '):
+                    nombre_archivo = mensaje.split(' ', 1)[1]
+                    self.subir_archivo(cliente_socket, nombre_archivo)
+                elif mensaje == 'salir':
+                    print(f'Cliente {direccion} desconectado.')
+                    break
+                else:
+                    cliente_socket.sendall('Comando no reconocido'.encode())
+        except Exception as e:
+            print(f'Error con {direccion}: {e}')
+        finally:
+            cliente_socket.close()
 
     def listar_archivos(self, cliente_socket):
-        archivos = os.listdir('archivos')
-        lista_archivos = '\n'.join(archivos)
+        archivos = os.listdir(self.directorio_archivos)
+        lista_archivos = '\n'.join(archivos) if archivos else 'No hay archivos disponibles'
         cliente_socket.sendall(lista_archivos.encode())
 
     def descargar_archivo(self, cliente_socket, nombre_archivo):
-        ruta_archivo = os.path.join('archivos', nombre_archivo)
-        if os.path.exists(ruta_archivo):
+        ruta_archivo = os.path.join(self.directorio_archivos, nombre_archivo)
+        if os.path.isfile(ruta_archivo):
+            cliente_socket.sendall('OK'.encode())  # Confirmación inicial
             with open(ruta_archivo, 'rb') as archivo:
-                datos = archivo.read()
-                cliente_socket.sendall(datos)
+                while chunk := archivo.read(1024):
+                    cliente_socket.sendall(chunk)
+            cliente_socket.sendall(b'FIN')  # Señal de terminación
         else:
             cliente_socket.sendall('Archivo no encontrado'.encode())
 
     def subir_archivo(self, cliente_socket, nombre_archivo):
-        ruta_archivo = os.path.join('archivos', nombre_archivo)
+        ruta_archivo = os.path.join(self.directorio_archivos, nombre_archivo)
+        cliente_socket.sendall('OK'.encode())  # Confirmación inicial
         with open(ruta_archivo, 'wb') as archivo:
             while True:
                 datos = cliente_socket.recv(1024)
-                if not datos:
+                if datos.endswith(b'FIN'):  # Señal para finalizar recepción
+                    archivo.write(datos[:-3])
                     break
                 archivo.write(datos)
         cliente_socket.sendall('Archivo subido correctamente'.encode())
 
     def iniciar(self):
         print(f'Servidor iniciado en {self.host}:{self.puerto}')
-        while True:
-            cliente_socket, direccion = self.socket_servidor.accept()
-            print(f'Conexión establecida con {direccion}')
-            hilo = threading.Thread(target=self.manejar_conexion, args=(cliente_socket,))
-            hilo.start()
+        if not os.path.exists(self.directorio_archivos):
+            os.makedirs(self.directorio_archivos)
+        try:
+            while True:
+                cliente_socket, direccion = self.socket_servidor.accept()
+                hilo = threading.Thread(target=self.manejar_conexion, args=(cliente_socket, direccion))
+                hilo.start()
+        except KeyboardInterrupt:
+            print('Servidor detenido manualmente.')
+        finally:
+            self.socket_servidor.close()
 
 
 if __name__ == '__main__':
